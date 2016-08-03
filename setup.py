f21932ee1c4d3e9451081bdf8be36f08bc92a7d4
Hugh Sorby <h.sorby@auckland.ac.nz>
2016-08-03 14:24:23 +1200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,18 +4,20 @@ import io
 # List all of your Python package dependencies in the
 # requirements.txt file
 
-def readfile(filename):
+def readfile(filename, split=False):
     with io.open(filename, encoding="utf-8") as stream:
-        return stream.read().split("\n")
+        if split:
+            return stream.read().split("\\n")
+        return stream.read()
 
-readme = readfile("README.rst")[3:]  # skip title
-requires = readfile("requirements.txt")
-licence = readfile("LICENSE")
+readme = readfile("README.rst", split=True)[3:]  # skip title
+requires = readfile("requirements.txt", split=True)
+license = readfile("LICENSE")
 
 setup(name='opencmiss.utils',
       version='0.1.0',
       description='Utilities for Python.',
-      long_description=readme + licence,
+      long_description='\n'.join(readme) + license,
       classifiers=[],
       author='Hugh Sorby',
       author_email='',
