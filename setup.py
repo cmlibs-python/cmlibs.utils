b3805fe7a752f20905046a15c3ad30a160d64686
Hugh Sorby <h.sorby@auckland.ac.nz>
2016-08-05 11:01:01 +1200

    setup.py clean up.

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ import io
 # List all of your Python package dependencies in the
 # requirements.txt file
 
-
 def readfile(filename, split=False):
     with io.open(filename, encoding="utf-8") as stream:
         if split:
@@ -15,18 +14,20 @@ readme = readfile("README.rst", split=True)[3:]  # skip title
 requires = readfile("requirements.txt", split=True)
 licence = readfile("LICENSE")
 
-setup(name='opencmiss.utils',
-      version='0.1.0',
-      description='Utilities for Python.',
-      long_description='\n'.join(readme) + licence,
-      classifiers=[],
-      author='Hugh Sorby',
-      author_email='',
-      url='',
-      license='APACHE',
-      packages=find_packages(exclude=['ez_setup',]),
-      namespace_packages=['opencmiss'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=requires,
+setup(
+    name='opencmiss.utils',
+    version='0.1.0',
+    description='OpenCMISS Utilities for Python.',
+    long_description='\n'.join(readme) + licence,
+    classifiers=[],
+    author='Hugh Sorby',
+    author_email='',
+    url='',
+    license='APACHE',
+    packages=find_packages(exclude=['ez_setup',]),
+    namespace_packages=['opencmiss'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requires,
 )
+
