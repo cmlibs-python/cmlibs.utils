4ff581ee13e06a32f3851ec1027827f592a5292b
Hugh Sorby <h.sorby@auckland.ac.nz>
2016-08-05 10:58:51 +1200

    Initial addition of common Python utilities.

--- opencmiss/utils/zinc.py
+++ opencmiss/utils/zinc.py
@@ -4,6 +4,10 @@ Created on May 21, 2015
 @author: hsorby
 '''
 from opencmiss.zinc.element import Element, Elementbasis
+from opencmiss.zinc.node import Node
+from opencmiss.zinc.field import Field
+from opencmiss.zinc.status import OK as ZINC_OK
+from opencmiss.utils.maths import vectorops
 
 
 def createFiniteElementField(region):
@@ -217,4 +221,135 @@ def createIsoScalarField(region, coordinate_field, plane):
     
     return iso_scalar_field
 
+
+def createVisibilityFieldForPlane(region, coordinate_field, plane):
+    """
+    Create a
+    :param region:
+    :param coordinate_field:
+    :param plane:
+    :return:
+    """
+    fieldmodule = region.getFieldmodule()
+    fieldmodule.beginChange()
+    normal_field = plane.getNormalField()
+    rotation_point_field = plane.getRotationPointField()
+    visibility_field = createPlaneVisibilityField(fieldmodule, coordinate_field, normal_field, rotation_point_field)
+    fieldmodule.endChange()
+
+    return visibility_field
+
+
+def defineStandardVisualisationTools(context):
+    glyph_module = context.getGlyphmodule()
+    glyph_module.defineStandardGlyphs()
+    material_module = context.getMaterialmodule()
+    material_module.defineStandardMaterials()
+
+
+def transformCoordinates(field, rotationScale, offset, time = 0.0):
+    '''
+    Transform finite element field coordinates by matrix and offset, handling nodal derivatives and versions.
+    Limited to nodal parameters, rectangular cartesian coordinates
+    :param field: the coordinate field to transform
+    :param rotationScale: square transformation matrix 2-D array with as many rows and columns as field components.
+    :param offset: coordinates offset
+    :return: True on success, otherwise false
+    '''
+    ncomp = field.getNumberOfComponents()
+    if ((ncomp != 2) and (ncomp != 3)):
+        print('zinc.transformCoordinates: field has invalid number of components')
+        return False
+    if (len(rotationScale) != ncomp) or (len(offset) != ncomp):
+        print('zinc.transformCoordinates: invalid matrix number of columns or offset size')
+        return False
+    for matRow in rotationScale:
+        if len(matRow) != ncomp:
+            print('zinc.transformCoordinates: invalid matrix number of columns')
+            return False
+    if (field.getCoordinateSystemType() != Field.COORDINATE_SYSTEM_TYPE_RECTANGULAR_CARTESIAN):
+        print('zinc.transformCoordinates: field is not rectangular cartesian')
+        return False
+    feField = field.castFiniteElement()
+    if not feField.isValid():
+        print('zinc.transformCoordinates: field is not finite element field type')
+        return False
+    success = True
+    fm = field.getFieldmodule()
+    fm.beginChange()
+    cache = fm.createFieldcache()
+    cache.setTime(time)
+    nodes = fm.findNodesetByFieldDomainType(Field.DOMAIN_TYPE_NODES)
+    nodetemplate = nodes.createNodetemplate()
+    nodeIter = nodes.createNodeiterator()
+    node = nodeIter.next()
+    while node.isValid():
+        nodetemplate.defineFieldFromNode(feField, node)
+        cache.setNode(node)
+        for derivative in [Node.VALUE_LABEL_VALUE, Node.VALUE_LABEL_D_DS1, Node.VALUE_LABEL_D_DS2, Node.VALUE_LABEL_D2_DS1DS2,
+                           Node.VALUE_LABEL_D_DS3, Node.VALUE_LABEL_D2_DS1DS3, Node.VALUE_LABEL_D2_DS2DS3, Node.VALUE_LABEL_D3_DS1DS2DS3]:
+            versions = nodetemplate.getValueNumberOfVersions(feField, -1, derivative)
+            for v in range(versions):
+                result, values = feField.getNodeParameters(cache, -1, derivative, v + 1, ncomp)
+                if result != ZINC_OK:
+                    success = False
+                else:
+                    newValues = vectorops.matrixvectormult(rotationScale, values)
+                    if derivative == Node.VALUE_LABEL_VALUE:
+                        newValues = vectorops.add(newValues, offset)
+                    result = feField.setNodeParameters(cache, -1, derivative, v + 1, newValues)
+                    if result != ZINC_OK:
+                        success = False
+        node = nodeIter.next()
+    fm.endChange()
+    if not success:
+        print('zinc.transformCoordinates: failed to get/set some values')
+    return success
+
+
+def create2DFiniteElement(fieldmodule, finite_element_field, node_coordinate_set):
+    '''
+    Create a single finite element using the supplied
+    finite element field and node coordinate set.
+    '''
+    # Find a special node set named 'nodes'
+    nodeset = fieldmodule.findNodesetByName('nodes')
+    node_template = nodeset.createNodetemplate()
+
+    # Set the finite element coordinate field for the nodes to use
+    node_template.defineField(finite_element_field)
+    field_cache = fieldmodule.createFieldcache()
+
+    node_identifiers = []
+    # Create eight nodes to define a cube finite element
+    for node_coordinate in node_coordinate_set:
+        node = nodeset.createNode(-1, node_template)
+        node_identifiers.append(node.getIdentifier())
+        # Set the node coordinates, first set the field cache to use the current node
+        field_cache.setNode(node)
+        # Pass in floats as an array
+        finite_element_field.assignReal(field_cache, node_coordinate)
+
+    # Use a 3D mesh to to create the 2D finite element.
+    mesh = fieldmodule.findMeshByDimension(2)
+    element_template = mesh.createElementtemplate()
+    element_template.setElementShapeType(Element.SHAPE_TYPE_SQUARE)
+    element_node_count = 4
+    element_template.setNumberOfNodes(element_node_count)
+    # Specify the dimension and the interpolation function for the element basis function
+    linear_basis = fieldmodule.createElementbasis(2, Elementbasis.FUNCTION_TYPE_LINEAR_LAGRANGE)
+    # the indecies of the nodes in the node template we want to use.
+    node_indexes = [1, 2, 3, 4]
+
+
+    # Define a nodally interpolated element field or field component in the
+    # element_template
+    element_template.defineFieldSimpleNodal(finite_element_field, -1, linear_basis, node_indexes)
+
+    for i, node_identifier in enumerate(node_identifiers):
+        node = nodeset.findNodeByIdentifier(node_identifier)
+        element_template.setNode(i + 1, node)
+
+    mesh.defineElement(-1, element_template)
+    fieldmodule.defineAllFaces()
 
--- setup.py
+++ setup.py
@@ -4,18 +4,21 @@ import io
 # List all of your Python package dependencies in the
 # requirements.txt file
 
-def readfile(filename):
-    with io.open(filename, encoding="utf-8") as stream:
-        return stream.read().split("\n")
 
-readme = readfile("README.rst")[3:]  # skip title
-requires = readfile("requirements.txt")
+def readfile(filename, split=False):
+    with io.open(filename, encoding="utf-8") as stream:
+        if split:
+            return stream.read().split("\n")
+        return stream.read()
+
+readme = readfile("README.rst", split=True)[3:]  # skip title
+requires = readfile("requirements.txt", split=True)
 licence = readfile("LICENSE")
 
 setup(name='opencmiss.utils',
       version='0.1.0',
       description='Utilities for Python.',
-      long_description=readme + licence,
+      long_description='\n'.join(readme) + licence,
       classifiers=[],
       author='Hugh Sorby',
       author_email='',
