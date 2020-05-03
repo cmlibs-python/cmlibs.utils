ec59a1c592902c5de80417ac70496dbd3689643e
Richard Christie <r.christie@auckland.ac.nz>
2020-05-03 15:35:57 +1200

    Fix transposed rotation matrix to/from Euler angles
    Tidied vectorops function names and docs.
    Incremented version.

--- src/opencmiss/utils/maths/vectorops.py
+++ src/opencmiss/utils/maths/vectorops.py
@@ -52,10 +52,12 @@ def div(u, c):
     return [u[i] / c for i in range(len(u))]
 
 
-def rotmx(quaternion):
+def quaternion_to_rotation_matrix(quaternion):
     '''
     This method takes a quaternion representing a rotation
-    and turns it into a rotation matrix. 
+    and turns it into a rotation matrix.
+    :return: 3x3 rotation matrix suitable for pre-multiplying vector v:
+    i.e. v' = Mv
     '''
     mag_q = magnitude(quaternion)
     norm_q = div(quaternion, mag_q)
@@ -67,40 +69,18 @@ def rotmx(quaternion):
     return mx
 
 
-def mxconstantmult(m, c):
+def matrixconstantmult(m, c):
     '''
     Multiply components of matrix m by constant c
     '''
     return [mult(row_m, c) for row_m in m]
 
 
-def mxvectormult(m, v):
+def matrixvectormult(m, v):
     '''
     Post multiply matrix m by vector v
     '''
     return [dot(row_m, v) for row_m in m]
-
-
-def vectormxmult(v, m):
-    '''
-    Premultiply matrix m by vector v
-    '''
-    rows = len(m)
-    if len(v) != rows:
-        raise ValueError('vectormatmult mismatched rows')
-    columns = len(m[0])
-    result = []
-    for c in range(0, columns):
-        result.append(sum(v[r]*m[r][c] for r in range(rows)))
-    return result
-
-
-def mxmult(a, b):
-    '''
-    Multiply 2 matrices: first index is down row, second is across column.
-    Assumes sizes are compatible (
-    '''
-    return [vectormxmult(row_a, b) for row_a in a]
 
 
 def vectormatrixmult(v, m):
@@ -109,7 +89,7 @@ def vectormatrixmult(v, m):
     '''
     rows = len(m)
     if len(v) != rows:
-        raise ValueError('vectormatmult mismatched rows')
+        raise ValueError('vectormatrixmult mismatched rows')
     columns = len(m[0])
     result = []
     for c in range(0, columns):
@@ -120,7 +100,7 @@ def vectormatrixmult(v, m):
 def matrixmult(a, b):
     '''
     Multiply 2 matrices: first index is down row, second is across column.
-    Assumes sizes are compatible (
+    Assumes sizes are compatible (number of columns of a == number of rows of b).
     '''
     return [vectormatrixmult(row_a, b) for row_a in a]
 
@@ -131,9 +111,17 @@ def transpose(a):
     return list(map(list, zip(*a)))
 
 
-def eulerToRotationMatrix3(euler_angles):
+def euler_to_rotation_matrix(euler_angles):
     '''
-    From OpenCMISS-Zinc graphics_library.cpp
+    From OpenCMISS-Zinc graphics_library.cpp, with matrix transposed to row major.
+    Matrix is product RzRyRx, giving rotation about x, then y, then z with
+    positive angles rotating by right hand rule about axis.
+    :param euler_angles: 3 angles in radians, components:
+    0 = azimuth (about z)
+    1 = elevation (about y)
+    2 = roll (about x)
+    :return: 3x3 rotation matrix suitable for pre-multiplying vector v:
+    i.e. v' = Mv
     '''
     cos_azimuth = cos(euler_angles[0])
     sin_azimuth = sin(euler_angles[0])
@@ -142,43 +130,52 @@ def eulerToRotationMatrix3(euler_angles):
     cos_roll = cos(euler_angles[2])
     sin_roll = sin(euler_angles[2])
     mat3x3 = [
-        [cos_azimuth*cos_elevation, sin_azimuth*cos_elevation, -sin_elevation],
-        [cos_azimuth*sin_elevation*sin_roll - sin_azimuth*cos_roll, sin_azimuth*sin_elevation*sin_roll + cos_azimuth*cos_roll, cos_elevation*sin_roll],
-        [cos_azimuth*sin_elevation*cos_roll + sin_azimuth*sin_roll, sin_azimuth*sin_elevation*cos_roll - cos_azimuth*sin_roll, cos_elevation*cos_roll]]
+        [cos_azimuth*cos_elevation, cos_azimuth*sin_elevation*sin_roll - sin_azimuth*cos_roll, cos_azimuth*sin_elevation*cos_roll + sin_azimuth*sin_roll],
+        [sin_azimuth*cos_elevation, sin_azimuth*sin_elevation*sin_roll + cos_azimuth*cos_roll, sin_azimuth*sin_elevation*cos_roll - cos_azimuth*sin_roll],
+        [-sin_elevation, cos_elevation*sin_roll, cos_elevation*cos_roll]]
     return mat3x3
 
 
-def rotationMatrix3ToEuler(matrix):
+def rotation_matrix_to_euler(matrix):
     '''
-    From OpenCMISS-Zinc graphics_library.cpp
+    From OpenCMISS-Zinc graphics_library.cpp, with matrix transposed to row major.
+    Inverse function to euler_to_rotation_matrix.
     '''
     MATRIX_TO_EULER_TOLERANCE = 1.0E-12
     euler_angles = [0.0, 0.0, 0.0]
-    if (fabs(matrix[0][0]) > MATRIX_TO_EULER_TOLERANCE) and (fabs(matrix[0][1]) > MATRIX_TO_EULER_TOLERANCE):
-        euler_angles[0] = atan2(matrix[0][1], matrix[0][0])
-        euler_angles[2] = atan2(matrix[1][2], matrix[2][2])
-        euler_angles[1] = atan2(-matrix[0][2], matrix[0][0]/cos(euler_angles[0]))
-    elif fabs(matrix[0][0]) > MATRIX_TO_EULER_TOLERANCE:
-        euler_angles[0] = atan2(matrix[0][1], matrix[0][0])
-        euler_angles[2] = atan2(matrix[1][2], matrix[2][2])
-        euler_angles[1] = atan2(-matrix[0][2], matrix[0][0]/cos(euler_angles[0]))
+    if fabs(matrix[0][0]) > MATRIX_TO_EULER_TOLERANCE:
+        euler_angles[0] = atan2(matrix[1][0], matrix[0][0])
+        euler_angles[2] = atan2(matrix[2][1], matrix[2][2])
+        euler_angles[1] = atan2(-matrix[2][0], matrix[0][0]/cos(euler_angles[0]))
     elif fabs(matrix[0][1]) > MATRIX_TO_EULER_TOLERANCE:
-        euler_angles[0] = atan2(matrix[0][1], matrix[0][0])
-        euler_angles[2] = atan2(matrix[1][2], matrix[2][2])
-        euler_angles[1] = atan2(-matrix[0][2], matrix[0][1]/sin(euler_angles[0]))
+        euler_angles[0] = atan2(matrix[1][0], matrix[0][0])
+        euler_angles[2] = atan2(matrix[2][1], matrix[2][2])
+        euler_angles[1] = atan2(-matrix[2][0], matrix[1][0]/sin(euler_angles[0]))
     else:
-        euler_angles[1] = atan2(-matrix[0][2],0) # get +/-1
+        euler_angles[1] = atan2(-matrix[2][0],0) # get +/-1
         euler_angles[0] = 0
-        euler_angles[2] = atan2(-matrix[2][1], -matrix[2][0]*matrix[0][2])
+        euler_angles[2] = atan2(-matrix[1][2], -matrix[0][2]*matrix[2][0])
     return euler_angles
 
 
-def axisAngleToQuaternion(axis, angle):
-    return [cos(angle/2), axis[0]*sin(angle/2), axis[1]*sin(angle/2), axis[2]*sin(angle/2)]
+def axis_angle_to_quaternion(axis, angle):
+    '''
+    :param axis: Unit vector axis of rotation.
+    :param angle: Angle of rotation in right hand sense around axis, in radians.
+    :return: Quaternion representing rotation.
+    '''
+    sin_half_angle = sin(angle/2)
+    return [cos(angle/2), axis[0]*sin_half_angle, axis[1]*sin_half_angle, axis[2]*sin_half_angle]
 
 
-def axisAngleToRotationMatrix(axis, angle):
-    pass
+def axis_angle_to_rotation_matrix(axis, angle):
+    '''
+    :param axis: Unit vector axis of rotation.
+    :param angle: Angle of rotation in right hand sense around axis, in radians.
+    :return: 3x3 rotation matrix suitable for pre-multiplying vector v:
+    i.e. v' = Mv
+    '''
+    return quaternion_to_rotation_matrix(axis_angle_to_quaternion(axis, angle))
 
 
 def reshape(a, new_shape):
@@ -199,3 +196,14 @@ def reshape(a, new_shape):
             b = flat_list
 
     return b
+
+
+rotmx = quaternion_to_rotation_matrix
+mxconstantmult = matrixconstantmult
+mxvectormult = matrixvectormult
+vectormxmult = vectormatrixmult
+mxmult = matrixmult
+eulerToRotationMatrix3 = euler_to_rotation_matrix
+rotationMatrix3ToEuler = rotation_matrix_to_euler
+axisAngleToQuaternion = axis_angle_to_quaternion
+axisAngleToRotationMatrix = axis_angle_to_rotation_matrix
